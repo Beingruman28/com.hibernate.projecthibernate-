e76fa61248f11e14525d7fe5fed338c1a5c7d2f2
ruman <mohammedruman1996@gmail.com>
2022-03-15 14:31:24 +0530

    hibernate program .

---
+++ ProjectHibernate/src/com/hibernate/projecthibernate/Users.java
@@ -0,0 +1,65 @@
+package com.hibernate.projecthibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class Users {
+	public Users(String user_fname, String user_lname, String user_email) {
+		super();
+		this.user_fname = user_fname;
+		this.user_lname = user_lname;
+		this.user_email = user_email;
+	}
+
+	@Id
+	@Column(name="user_id")
+	int user_id;
+	
+	@Column(name="user_fname")
+	String user_fname;
+	
+	@Column(name="user_lname")
+	String user_lname;
+	
+	@Column(name="user_email")
+	String user_email;
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUser_fname() {
+		return user_fname;
+	}
+
+	public void setUser_fname(String user_fname) {
+		this.user_fname = user_fname;
+	}
+
+	public String getUser_lname() {
+		return user_lname;
+	}
+
+	public void setUser_lname(String user_lname) {
+		this.user_lname = user_lname;
+	}
+
+	public String getUser_email() {
+		return user_email;
+	}
+
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+	
+	
+
+}
